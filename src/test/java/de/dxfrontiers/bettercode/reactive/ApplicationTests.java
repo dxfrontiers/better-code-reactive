a9ce53bbd61a7f1fefc0a94dc4607167948760af
Benedikt Jerat <b.jerat@googlemail.com>
2023-05-26 12:35:36 +0200

    Initial setup

---
+++ src/test/java/de/dxfrontiers/bettercode/reactive/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dxfrontiers.bettercode.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
